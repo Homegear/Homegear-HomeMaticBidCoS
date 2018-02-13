f13ee97e937b7ed77bd4b88a7305b21bbd066bdd
Sathya Laufer <sathya@laufers.net>
2018-02-13 17:25:05 +0100

    Create dummy default interface when there is none

--- src/Interfaces.cpp
+++ src/Interfaces.cpp
@@ -79,6 +79,7 @@ void Interfaces::create()
 				if(i->second->isDefault || !GD::defaultPhysicalInterface) GD::defaultPhysicalInterface = device;
 			}
 		}
+        if(!GD::defaultPhysicalInterface) GD::defaultPhysicalInterface = std::make_shared<IBidCoSInterface>(std::make_shared<BaseLib::Systems::PhysicalInterfaceSettings>());
 	}
 	catch(const std::exception& ex)
 	{
